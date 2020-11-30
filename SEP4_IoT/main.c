bf14054ea58d086c80263efe9672eb22fdbc14fa
Toma Chan <tomachan.psc@gmail.com>
2020-11-30 22:12:04 +0100

    added global interrupt

--- SEP4_IoT/main.c
+++ SEP4_IoT/main.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <ATMEGA_FreeRTOS.h>
 #include <FreeRTOSTraceDriver.h>
 #include <stdio_driver.h>
@@ -17,6 +18,7 @@ int main(void)
 {
 	trace_init();
 	stdio_create(ser_USART0);
+	sei();
 	printf("Program executed! \n");
 	createMainTask();                // Creates the main task
 	vTaskStartScheduler();           // Initialize and run the freeRTOS scheduler. Execution should never return from here.
